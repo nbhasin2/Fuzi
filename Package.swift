5377a962aba8cbdb7fc801fe743e8aad1954196a
Nishant Bhasin <nikieme3@gmail.com>
2022-01-04 15:51:10 -0500

    Update dependencies [SPM]

--- Package.swift
+++ Package.swift
@@ -6,7 +6,12 @@ import PackageDescription
 let package = Package(
     name: "Fuzi",
     products: [
-        .library(name: "Fuzi", targets: ["Fuzi"]),
+        .library(
+            name: "Fuzi",
+            targets: ["Fuzi"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
         .target(name: "Fuzi",
